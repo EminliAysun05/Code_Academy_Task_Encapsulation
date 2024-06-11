ee5bca9dc87f8640cd5a2b50cb163273ef3eb0bc
EminliAysun05 <aeminli232@gmail.com>
2024-06-11 18:37:03 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_Academy_Task_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
