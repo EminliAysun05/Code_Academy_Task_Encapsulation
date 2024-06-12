9becca62ccea82141ad4d9995cb7e1be90ad5bda
EminliAysun05 <aeminli232@gmail.com>
2024-06-12 13:01:11 +0400

    End

--- Program.cs
+++ Program.cs
@@ -1,10 +1,82 @@
 ﻿namespace Code_Academy_Task_Encapsulation
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            User[] users = new User[3];
+
+           
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine("Fullname: ");
+                string Fullname = Console.ReadLine();
+
+                Console.WriteLine("Email: ");
+                string Email = Console.ReadLine();
+
+                string password;
+                bool IsValidPassWord = false;
+                while (!IsValidPassWord)
+                {
+                    Console.WriteLine("Password: ");
+                    string Password = Console.ReadLine();
+
+                    IsValidPassWord = User.PassWordChecker(Password);
+                    if (!IsValidPassWord)
+                    {
+                        Console.WriteLine("Error password");
+                    }
+                    else
+                    {
+                        users[i] = new User(Fullname, Email, Password);
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            bool program = false;
+            while(program)
+            {
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Show all students: ");
+                Console.WriteLine("2. Get info by id: ");
+                Console.WriteLine("0. Program terminated");
+                Console.WriteLine("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                if(choice == 1)
+                {
+                    Console.WriteLine("All students: ");
+                    foreach (var user in users)
+                    {
+                        Console.WriteLine(user.GetInfo());
+                    }
+                }
+                else if(choice == 2) {
+                    Console.WriteLine("Enter the Id: ");
+                    int id = int.Parse(Console.ReadLine()); 
+
+                    User user = User.FindUserById(id,users);
+                    if(user != null) {
+                        Console.WriteLine(user.GetInfo());
+                    }
+                    else
+                    {
+                        Console.WriteLine("user not found");
+                    }
+                }
+
+                else if(choice == 0) {
+                    Console.WriteLine("program termianted");
+                    program = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. ");
+                }
+            }
+            
         }
     }
 }
---
+++ User.cs
@@ -0,0 +1,66 @@
+﻿
+
+namespace Code_Academy_Task_Encapsulation
+{
+   public class User
+    {
+        private static int NextId=1;
+        public int Id { get;  }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public User(string fullname, string email, string password)
+        {
+            Id = NextId++;
+            this.Email = email;
+            this.Fullname = fullname;
+            this.Password = password;
+        }
+      public   static bool PassWordChecker(string password)
+        {
+            if (password.Length < 8)
+            {
+                return false;
+            }
+            bool HasUpperCase = false;
+            bool HasLowerCase = false;
+            bool HasDigit = false;
+           foreach (char c in password) { 
+            if(char.IsUpper(c))
+                {
+                    HasUpperCase = true;
+                }
+           else if(char.IsLower(c))
+                {
+                    HasLowerCase = true;
+                }
+              else  if (char.IsDigit(c))
+                {
+                    HasDigit = true;
+                }
+            }
+           return HasDigit&&HasLowerCase&&HasUpperCase;
+            }
+
+        public string GetInfo()
+        {
+            return $"{Id},{Fullname},{Email}"; 
+
+        }
+        public static  User FindUserById (int id, User[] users)
+        {
+            foreach (var user in users)
+            {
+                if (user.Id == id)
+                {
+                    return user;
+                }
+            }
+            return new User("not found","not found","not found");
+        }
+           
+
+        }
+   
+    }
+
